6597efcdaad6b794576d37847095cecf839f6a6d
ChunjunYang <yangchunjun1994@163.com>
2016-08-31 18:24:17 +0800

    backup

---
+++ MyStruct/MyStruct/struct.go
@@ -0,0 +1,8 @@
+package MyStruct
+
+type Conn struct{
+    c int
+    a int
+    CC int
+    AA int
+}
--- conn.go
+++ conn.go
@@ -4,10 +4,11 @@ import (
 	"github.com/garyburd/redigo/redis"
 )
 
+//var conn redis.Conn
 func newPool() *redis.Pool {
 	return &redis.Pool{
 		MaxIdle:   80,
-		MaxActive: 20000,
+		MaxActive: 200,
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.Dial("tcp", "localhost:6379")
 			if err != nil {
@@ -20,27 +21,27 @@ func newPool() *redis.Pool {
 
 var pool = newPool()
 
-func getConn() redis.Conn {
+/*
+func Init()error{
+    pool = newPool()
+}
+*/
+
+func getConn() redis.Pool {
 	c := pool.Get()
 	return c
 }
 
-func releaseConn() error {
-	return nil
+func (c *redis.Pool) releaseConn() {
+	c.Close()
 }
 
 func DoGet(userId string) {
-	c := pool.Get()
-	c.Do("INCR", userId)
-	defer c.Close()
-}
-
-func GetActiveConnNum() int {
-	return pool.ActiveCount()
+	c := getConn()
+	defer c.releaseConn()
 }
 
 func PipeGet(userId string) {
-	c := pool.Get()
-	c.Send("INCR", userId)
-	defer c.Close()
+}
+func Release() {
 }
